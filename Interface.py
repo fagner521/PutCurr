c77ea5d764307a58adb34124c1cb613d2c0dba24
fagneralc <fagner.all@hotmail.com>
2020-03-22 23:51:56 -0300

    Amostra da filtragem

--- Interface.py
+++ Interface.py
@@ -25,12 +25,29 @@ from sys import argv as arg
 print("-n Nome Completo\n -d Cidade \n -p profissão\n -c Telefone \n -q Email\n -b Arquivo")
 argumentos = arg[1:]
 print(argumentos)
-nome_completo = "-a"
+index_nome = argumentos.index("-n") +1
+nome = []
+numerador = 0
+for palavra in argumentos[index_nome:]:
+    numerador = numerador + 1
+    print(numerador)
+    bolean = "-" in palavra
+    if bolean == True:
+        print('Saindo')
+        break
+    else:
+        print("listado")
+        nome.append(str(palavra))
 
+print(nome)
+quit()
+
+
+'''''''''
 while (nome_completo != "-d")
     index_nome = argumentos.index("-n") + 1
     nome_completo = argumentos[index_nome]
     print(nome_completo)
 Cidade = argumentos.index("-d") + 1
 print(Cidade)
-
+'''''''''
