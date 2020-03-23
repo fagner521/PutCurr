1034c75ea473df778e80d6c3a585e966c9046686
fagneralc <fagner.all@hotmail.com>
2020-03-22 22:58:57 -0300

    Repetido

--- Interface.py
+++ Interface.py
@@ -4,7 +4,11 @@ print("-n Nome\n -d Aquivo\n -p profissão\n")
 argumentos = arg[1:]
 print(argumentos)
 profissao = argumentos.index("-p") + 1
+<<<<<<< HEAD
 print(argumentos[profissao])
 
 nome = sys.argv[2]
 print(nome)
+=======
+print(argumentos[profissao])
+>>>>>>> f888ac8... Sys e menu basico
